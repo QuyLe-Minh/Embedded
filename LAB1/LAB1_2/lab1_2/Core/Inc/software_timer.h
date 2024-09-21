36f5ca1ffe0d4446e39c46335c8cea8be840c20e
leminhquy <leminhquytp@gmail.com>
2024-09-21 09:02:10 +0700

    init repo

---
+++ LAB1/LAB1_2/lab1_2/Core/Inc/software_timer.h
@@ -0,0 +1,24 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 24, 2023
+ *      Author: HaHuyen
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "tim.h"
+
+extern uint16_t flag_timer2;
+extern uint16_t flag_timer1;
+extern uint16_t flag_timer3;
+
+void timer_init();
+void setTimer1(uint16_t duration);
+void setTimer2(uint16_t duration);
+void setTimer3(uint16_t duration);
+
+//void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim );
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
